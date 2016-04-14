4f743de66486d005272c221983f931daad0ae7e6
Yuka Sato <yuka.sato@gaiax.com>
2016-04-14 15:57:25 +0900

    green IntegerValidator本実装

--- app/integer_validator.rb
+++ app/integer_validator.rb
@@ -1,10 +1,17 @@
 class IntegerValidator
 
   def initialize(string)
+    @string = string
   end
 
   def valid
-    true
+    if @string == "0"
+      return true
+    end
+    if @string.to_i != 0
+      return true
+    end
+    false
   end
 end
 
--- spec/integer_validator_spec.rb
+++ spec/integer_validator_spec.rb
@@ -5,4 +5,14 @@ describe IntegerValidator do
     integer_validar = described_class.new("1")
     expect(integer_validar.valid).to be_truthy
   end
+
+  it do
+    integer_validar = described_class.new("A")
+    expect(integer_validar.valid).to be_falsy
+  end
+  
+  it do
+    integer_validar = described_class.new("0")
+    expect(integer_validar.valid).to be_truthy
+  end
 end
