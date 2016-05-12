0b01895e6ee24b34385534f8db92444bf1bdde86
Yuka Sato <yuka.sato@gaiax.com>
2016-05-12 14:37:32 +0900

    FizzbuzzCommand のテスト実装

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -27,39 +27,4 @@ describe Interface do
       expect(interface.start("100")).to be_a(NilCommand)
     end
   end
-
-  # let(:inputer) { InputerStub.new(input) }
-  # let(:printer) { PrinterSpy.new }
-  # let(:logger) { Logger.new }
-  # let(:interface) { Interface.new(inputer, printer, logger) }
-  #
-  # context 'メニュー選択で1を選択した場合' do
-  #   context '正常系' do
-  #     let(:input) { "3" }
-  #
-  #     it do
-  #       interface.start("1")
-  #       expect(logger.all).to eq(["3 : Fizz"])
-  #     end
-  #   end
-  #
-  #   context 'FizzBuzzに整数以外の値を入力した場合' do
-  #     let(:input) { "A" }
-  #
-  #     it do
-  #       interface.start("1")
-  #       expect(logger.all).to be_empty
-  #     end
-  #   end
-  # end
-  #
-  # context 'メニューにない数字を選択した場合' do
-  #   let(:input) { "" }
-  #
-  #   it do
-  #     interface.start("100")
-  #     expect(printer.result).to be_empty
-  #   end
-  # end
-
 end
